c3f8f939020070c6ce4e890bedafe1a5bcd6aff0
Milan <mistikone1@gmail.com>
2020-06-26 13:07:17 +0200

    filters and mixins

---
+++ src/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Filters and mixins</h1>
+    <input type="text" v-model="filterText" />
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from "./fruitMixin";
+export default {
+  mixins: [fruitMixin]
+};
+</script>
+
+<style>
+</style>
